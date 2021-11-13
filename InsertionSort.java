dbd2476785cba37bbaf39e4653e029b99ea8b5ff
Bruno Almeida Kotesky <blasterplayersbr@gmail.com>
2021-11-13 20:30:04 -0300

    Algoritimos corrigidos

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,17 +1,21 @@
 public class InsertionSort {
     private static SortUtils utils = new SortUtils();
-    
-    public void sort(int[] arr) {
-        for (int i = 1; i < arr.length; i++) {
-            int j = i;
-            utils.comparacoes++;
-            while (j > 0 && arr[j - 1] > arr[j]) {
-                if(arr[j-1]>arr[j])
-                    utils.comparacoes++;
-                utils.troca(arr, i, j);
-                j--;
+
+    public static int[] sort(int arr[]) {
+        for (int i=1; i < arr.length; ++i) {
+            int key = arr[i];
+            int j = i-1;
+            while (j >=0 && arr[j] > key) {
+                utils.comparacoes++;
+                arr[j+1] = arr[j];
+                j = j-1;
+                utils.trocas++;
             }
+            arr[j+1] = key;
+            utils.trocas++;
         }
         utils.imprimir("InsertionSort");
+        return arr;
     }
+
 }
--- QuickSort.java
+++ QuickSort.java
@@ -1,30 +1,47 @@
 
 public class QuickSort {
     private static SortUtils utils = new SortUtils();
+
     public QuickSort(int[] array) {
         QuickSort.sort(array, 0, array.length - 1);
         utils.imprimir("Quick Sort");
     }
-    
+
     public static void sort(int[] arr, int low, int high) {
         if (utils.comparaMenor(low, high)) {
-            int pivot = partition(arr, low, high);
+            int pivot = dividir(arr, low, high);
             sort(arr, low, pivot - 1);
             sort(arr, pivot + 1, high);
         }
     }
 
-    private static int partition(int[] arr, int low, int high) {
-        int pivot = arr[high];
-        int i = low - 1;
-        for (int j = low; j < high; j++) {
-            if (arr[j] <= pivot) {
+    private static int dividir(int[] vetor, int inicio, int fim) {
+        int pivo, pontEsq, pontDir = fim;
+        pontEsq = inicio + 1;
+        pivo = vetor[inicio];
+        while (pontEsq <= pontDir) {
+            utils.comparacoes++;
+
+            while (pontEsq <= pontDir && vetor[pontEsq] <= pivo) {
                 utils.comparacoes++;
-                i++;
-                utils.troca(arr, i, j);
+                pontEsq++;
+            }
+
+            while (pontDir >= pontEsq && vetor[pontDir] > pivo) {
+                utils.comparacoes++;
+                pontDir--;
+            }
+
+            if (pontEsq < pontDir) {
+                utils.comparacoes++;
+                utils.troca(vetor, pontDir, pontEsq);
+                pontEsq++;
+                pontDir--;
             }
         }
-        utils.troca(arr, i + 1, high);
-        return i + 1;
-    }    
+
+        utils.troca(vetor, inicio, pontDir);
+        return pontDir;
+    }
+
 }
